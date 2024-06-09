907802ed1bb292778ab979ff6eb86191e04d4e2a
MahmudulHassan5809 <mahmudul.hassan240@gmail.com>
2024-06-09 17:25:33 +0600

    feat: wip sort by

--- src/main.py
+++ src/main.py
@@ -23,11 +23,15 @@ def list_repos(
     output: OutputFormat = typer.Option(
         OutputFormat.JSON, "--output", "-o", help="output format"
     ),
-    query: str = typer.Option(None, "--query", "-q", help="Query with jmespath")
+    query: str = typer.Option(None, "--query", "-q", help="Query with jmespath"),
+    sort_by: str = typer.Option(None, "--sort", "-s", help="Sort by keys"),
 ) -> None:
     repos = GitHubAPI(username=user).get_user_repositories()
     if query:
         repos = jmespath.search(query, repos)
+    if sort_by:
+        expr = f"sort_by(@, &{sort_by}).reverse(@)"
+        repos = jmespath.search(expr, repos)
     if repos:
         printer = DataPrinter(data=repos)
         printer.print_beauty(output=output)
