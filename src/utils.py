aea80220f22595a0c6d2037bed3988fede1f5795
MahmudulHassan5809 <mahmudul.hassan240@gmail.com>
2024-06-09 15:53:43 +0600

    feat: table output implemented

--- src/utils.py
+++ src/utils.py
@@ -3,7 +3,7 @@ import sys
 from typing import Any
 
 from constants import OutputFormat
-from rich import print_json
+from rich import console, print_json, table
 
 
 def print_beauty(data: list[dict[Any, Any]], output: OutputFormat) -> None:
@@ -14,3 +14,14 @@ def print_beauty(data: list[dict[Any, Any]], output: OutputFormat) -> None:
         writer.writerows(data)
     elif output == OutputFormat.JSON:
         print_json(data=data)
+    elif output == OutputFormat.TABLE:
+        tbl = table.Table()
+        headers = data[0].keys()
+        for h in headers:
+            tbl.add_column(str(h), justify="right", style="cyan", no_wrap=True)
+
+        for repo in data:
+            tbl.add_row(*[str(r) for r in repo.values()])
+
+        cnsl = console.Console()
+        cnsl.print(tbl)
